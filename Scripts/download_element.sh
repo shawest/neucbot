731a8fb83d224388ca98163233006bf8e70b20e4
Shawn Westerdale <shawest@physics.carleton.ca>
2016-12-06 20:37:14 -0500

    updated download_element

--- Scripts/download_element.sh
+++ Scripts/download_element.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 inp=$1
 _ele=${inp%%[0-9]*}
-ele=${_ele,,}
-Ele=${_ele^}
+ele=`echo ${_ele} | tr '[:upper:]' '[:lower:]'`
+ELE=`echo ${_ele} | tr '[:lower:]' '[:upper:]'`
+Ele=${ELE:0:1}${ele:1:10}
 dir=$PWD
 cd ./Data/Isotopes/
 git clone https://github.com/neucbot-datasets/${ele}.git 
