f8f650d1db16f5a2cd0ca586eb7061413a9cdd1b
Ivan <iv.gonch.0907@gmail.com>
2025-02-17 12:20:07 +0300

    Updated script to download JENDL data. All JendlOut data avaliable now.

--- Scripts/download_jendl_data.sh
+++ Scripts/download_jendl_data.sh
@@ -6,7 +6,14 @@
 
 dir=$PWD
 cd ./Data/Isotopes/
-git clone https://github.com/neucbot-datasets/jendl_data.git
-tar -xvzf ./jendl_data/jendl_an_totalxs.tar.gz
+git clone https://github.com/iv-gonch/jendl_data
+tar -xvzf ./jendl_data/B.tar.gz
+tar -xvzf ./jendl_data/Be.tar.gz
+tar -xvzf ./jendl_data/C.tar.gz
+tar -xvzf ./jendl_data/F.tar.bz2
+tar -xvzf ./jendl_data/Li.tar.gz
+tar -xvzf ./jendl_data/N.tar.gz
+tar -xvzf ./jendl_data/Na.tar.bz2
+tar -xvzf ./jendl_data/O.tar.bz2
 rm -rf jendl_data
 cd $dir
