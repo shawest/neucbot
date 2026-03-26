56febfda4a1798f69db8750ac3094bb3a5c4811b
Matt Piercy <mpiercy@gmail.com>
2026-03-26 09:15:44 -0700

    Fix NeucbotRunner test with dummy values to verify functionality

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -5,11 +5,19 @@ from unittest.mock import call, patch
 
 import numpy
 
-from neucbot import alpha, config, material, runner
+from neucbot import alpha, config, material, runner, utils
 
 
 class TestNeucbotRunner(TestCase):
-    def test_run(self):
+    @patch.object(material.Isotope, "cross_section", return_value=1e-27)
+    @patch.object(utils.Histogram, "rebin", return_value=utils.Histogram({1000: 1}))
+    @patch.object(
+        material.Isotope, "differential_n_spec", return_value=utils.Histogram()
+    )
+    @patch.object(material.Composition, "stopping_power", return_value=100)
+    def test_run(
+        self, mocked_stop_power, mocked_diff_n_spec, mocked_rebin, mocked_cross_sect
+    ):
         cfg = config.Config({})
         neucbot = runner.NeucbotRunner(cfg)
 
@@ -20,12 +28,12 @@ class TestNeucbotRunner(TestCase):
 
         expected = {
             "cross_sections": {
-                "C12": numpy.float64(0.0),
-                "H1": numpy.float64(0.0),
-                "O16": numpy.float64(0.0),
+                "C12": pytest.approx(1.517498e-06),
+                "H1": pytest.approx(9.104992e-06),
+                "O16": pytest.approx(5.690620e-07),
             },
-            "spectra_totals": {},
-            "total_cross_section": numpy.float64(0.0),
+            "spectra_totals": {1000: pytest.approx(1.119155e22)},
+            "total_cross_section": pytest.approx(1.119155e-05),
         }
 
         assert neucbot.run(alpha_list, comp) == expected
